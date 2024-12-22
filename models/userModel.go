972f7385785c371b8d3d82d7da4885d2c4dff752
RhoNit <ranitbiswas.cs@gmail.com>
2024-12-22 14:23:28 +0530

    added User model and req/resp models too

---
+++ models/userModel.go
@@ -0,0 +1,20 @@
+package models
+
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+type UserLoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserCreationResponse struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+}
